dfa3de44dadb4b0a12315bb695f0aff1b5561021
HEGEDUS Zoltan <hz@muszaki.info>
2011-06-22 14:40:14 +0200

    working randomizer and poller

--- poller.rb
+++ poller.rb
@@ -5,15 +5,29 @@ require 'nokogiri'
 require 'mysql2'
 
 def poller 
-#  db = SQLite3::Database.new "shopguard.db"
   db = Mysql2::Client.new(:host => 'localhost', :username => 'shop', :password => '123p4ss', :database => 'shopguard')
   f = File.open('status.xml')
   doc = Nokogiri::XML(f)
   f.close
   sensors = doc.xpath('//UPRAISED/*')
-  t = Time.now.to_i.to_s
+  t = Time.new.to_i.to_s
   
   q = "SELECT * FROM upraised WHERE timestamp = (SELECT MAX(timestamp) AS timestamp FROM upraised)"
+  q = "SELECT 
+         u1.sensor_id, 
+         u1.direction, 
+         u1.timestamp 
+       FROM
+         upraised u1 
+         JOIN (
+           SELECT 
+             sensor_id, 
+             MAX(timestamp) AS ts 
+           FROM 
+             upraised 
+           GROUP BY 
+             sensor_id) u2 ON u1.sensor_id = u2.sensor_id AND u1.timestamp = u2.ts"
+             
   res = db.query(q)
   previous_sensors = []
   res.each { |r|
@@ -21,7 +35,9 @@ def poller
   }
   sensors.each do |s|
     data = [s.name.gsub(/Sensor/, ''), s.text=='OFF'?'0':'1', t]
-    if previous_sensors.any? { |ps| ps[:sensor_id].to_s == s.name.gsub(/Sensor/, '') and ps[:direction].to_s != data[1] }
+    if previous_sensors.detect { |ps| 
+      ps['sensor_id'].to_s == s.name.gsub(/Sensor/, '') and ps['direction'].to_s != data[1] 
+    }
       q = "INSERT INTO upraised (sensor_id, direction, timestamp) VALUES (" + data[0] + ", " + data[1] + ", " + data[2] + ")"
       db.query(q)
    end
--- randomizer.rb
+++ randomizer.rb
@@ -18,11 +18,7 @@ def sensor_xml sensors
 end
 
 # only one of the sensors should change
-# there is a 50% chance to skip the change
 def random_action sensors
-  if rand(100) > 50
-    return sensors
-  end
   change = rand(16)
   sensors[change] = (1 - sensors[change]).abs
   sensors
@@ -32,10 +28,11 @@ def main
  sensors = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
  while true
    sensors = random_action sensors
+   xml = sensor_xml(sensors)
    File.open('status.xml', 'w') {|f| 
-     f.write( sensor_xml(sensors)) 
+     f.write(xml) 
    }
-   sleep rand(20)+5
+   sleep rand(15)
  end
 end
 

e39503fdf23f32118fc7ab7c6e0b0021c31f8e7c
HEGEDUS Zoltan <hz@muszaki.info>
2011-06-22 13:39:13 +0200

    v0.2

--- poller.rb
+++ poller.rb
@@ -17,19 +17,11 @@ def poller
   res = db.query(q)
   previous_sensors = []
   res.each { |r|
-    puts r.inspect
     previous_sensors << r
   }
-#  previous_sensors = res.first
-#  puts previous_sensors.inspect
   sensors.each do |s|
     data = [s.name.gsub(/Sensor/, ''), s.text=='OFF'?'0':'1', t]
     if previous_sensors.any? { |ps| ps[:sensor_id].to_s == s.name.gsub(/Sensor/, '') and ps[:direction].to_s != data[1] }
-      puts 'difi:'
-#      puts ps[:sensor_id].to_s.inspect
-      puts s.name.gsub(/Sensor/, '').inspect
- #     puts ps[:direction].to_s.inpsect
-      puts data[1].inspect
       q = "INSERT INTO upraised (sensor_id, direction, timestamp) VALUES (" + data[0] + ", " + data[1] + ", " + data[2] + ")"
       db.query(q)
    end
@@ -37,18 +29,6 @@ def poller
   sensors
 end
 
-def db_init
-  db = SQLite3::Database.new "shopguard.db"
-  # Create a database
-  rows = db.execute <<-SQL
-    create table upraised (
-      id INTEGER PRIMARY KEY,
-      sensor_id INTEGER,
-      direction INTEGER,
-      timestamp INTEGER
-    );
-  SQL
-end
 
 def main
   sensor_data = nil
--- randomizer.rb
+++ randomizer.rb
@@ -17,8 +17,12 @@ def sensor_xml sensors
   builder.to_xml
 end
 
-
+# only one of the sensors should change
+# there is a 50% chance to skip the change
 def random_action sensors
+  if rand(100) > 50
+    return sensors
+  end
   change = rand(16)
   sensors[change] = (1 - sensors[change]).abs
   sensors
@@ -28,10 +32,12 @@ def main
  sensors = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
  while true
    sensors = random_action sensors
-   xml = sensor_xml sensors
-   File.open('status.xml', 'w') {|f| f.write(xml) }
-   sleep 5
+   File.open('status.xml', 'w') {|f| 
+     f.write( sensor_xml(sensors)) 
+   }
+   sleep rand(20)+5
  end
 end
 
+#main loop started
 main
